0137d30da24cf0f4a677cc8932383ee930083fb0
SANJANADHAkSHAYENYG <138121675+SANJANADHAkSHAYENYG@users.noreply.github.com>
2023-06-30 15:37:00 +0530

    Add files via upload
    Using this code you can get input from the user and find the data type like number,string and decimal number

---
+++ data type.py
@@ -0,0 +1,8 @@
+#Variable declaration by getting input from the ueser
+a=int(input("Emter the number:"))
+b=str(input("Enter a word:"))
+c=float(input("Enter a decimal number:"))
+#Use "type" function to get the data type name
+print("Data type of a is:",type(a))
+print("Data type of b is:",type(b))
+print("Data type of c is:",type(c))
