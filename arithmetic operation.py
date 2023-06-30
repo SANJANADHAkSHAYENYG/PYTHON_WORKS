e1396657de4fc919a53685d7982e1e165ffb3fc5
SANJANADHAkSHAYENYG <138121675+SANJANADHAkSHAYENYG@users.noreply.github.com>
2023-06-30 10:35:27 +0530

    Add files via upload
    In this project you can give input and get output for basic arithmetic operations like addition,subtraction,multiplication and division

---
+++ arithmetic operation.py
@@ -0,0 +1,18 @@
+#Declaration of variable x and y
+#Getting input from the user
+x=int(input("Enter the value:"))
+y=int(input("Enter the value:"))
+#Use "a" variable to add the values
+a=x+y
+print("Sum of x and y value is:",a)
+#Use "b" variable to subtract the values
+b=x-y
+print("Differences of x and y value is:",b)
+#Use "c" variable to multiple the values
+c=x*y
+print("Multiplication of x and y value is:",c)
+#Use "d" variable to divide the values
+d=x/y
+print("Quotient of x and y value is:",d)
+
+
